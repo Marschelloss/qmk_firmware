a0a1d14d55709bbdeaf052420fae6699743f8cfb
Marcel Keßler <marcel.kessler@datev.de>
2023-11-27 11:10:02 +0100

    Changed num layer

--- keyboards/tominabox1/le_chiffre/keymaps/donhansdampf/keymap.c
+++ keyboards/tominabox1/le_chiffre/keymaps/donhansdampf/keymap.c
@@ -33,12 +33,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q, KC_W, KC_E, KC_R,  KC_T,       KC_NO, KC_Y,    KC_U, KC_I,    KC_O,   KC_P,
         KC_A, KC_S, KC_D, KC_F,  KC_G,              KC_H,    KC_J, KC_K,    KC_L,   SS_QUOT,
         KC_Z, KC_X, KC_C, KC_V,  KC_B,              KC_N,    KC_M, KC_COMM, KC_DOT, KC_SLSH,
-                          MO(2), LT(1, KC_SPC),     KC_LSFT, KC_BSPC
+                          MO(1), KC_SPC,            KC_LSFT, MO(2),
     ),
 	[_NUM] = LAYOUT(
-        KC_TRNS,       KC_TRNS,       KC_TAB,        KC_TRNS,       KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_DEL, 
-        OSM(MOD_LGUI), OSM(MOD_LALT), OSM(MOD_LCTL), OSM(MOD_LSFT), LGUI(KC_SPC),          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS,   
-        KC_TRNS,       KC_TRNS,       KC_TRNS,       KC_TRNS,       KC_TRNS,               KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_TRNS,  
+        KC_TRNS,       KC_TRNS,       KC_TAB,        KC_TRNS,       KC_TRNS,      KC_TRNS, KC_0,    KC_1,    KC_2,    KC_3,    KC_4,
+        OSM(MOD_LGUI), OSM(MOD_LALT), OSM(MOD_LCTL), OSM(MOD_LSFT), KC_TRNS,               KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_BSPC,
+        KC_TRNS,       KC_TRNS,       KC_TRNS,       KC_TRNS,       KC_TRNS,               KC_9,    KC_5,    KC_6,    KC_7,    KC_8,
                                                      KC_TRNS,       KC_TRNS,               KC_TRNS, KC_TRNS
     ),
 	[_NAV] = LAYOUT(
