990a1eba0aa556bb042e207574b579f8b96b5e3a
Marcel Keßler <marcel.kessler@datev.de>
2023-11-28 15:55:34 +0100

    Changed Num Layout once again

--- keyboards/tominabox1/le_chiffre/keymaps/donhansdampf/keymap.c
+++ keyboards/tominabox1/le_chiffre/keymaps/donhansdampf/keymap.c
@@ -24,6 +24,7 @@ enum layers {
 
 enum custom_keycodes {
     SS_QUOT = SAFE_RANGE,
+    SS_DQT,
     SS_UE,
     SS_AE,
     SS_OE,
@@ -35,19 +36,19 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
         KC_Q, KC_W, KC_E, KC_R,  KC_T,       KC_NO, KC_Y,    KC_U, KC_I,    KC_O,   KC_P,
-        KC_A, KC_S, KC_D, KC_F,  KC_G,              KC_H,    KC_J, KC_K,    KC_L,   SS_QUOT,
+        KC_A, KC_S, KC_D, KC_F,  KC_G,              KC_H,    KC_J, KC_K,    KC_L,   KC_BSPC,
         KC_Z, KC_X, KC_C, KC_V,  KC_B,              KC_N,    KC_M, KC_COMM, KC_DOT, KC_SLSH,
                           MO(1), KC_SPC,            KC_LSFT, MO(2)
     ),
 	[_NUM] = LAYOUT(
-        KC_TRNS,       KC_TRNS,       KC_TAB,        KC_TRNS,       KC_TRNS,      KC_NO,   KC_NO,   KC_1,    KC_2,    KC_3,    KC_4,
-        OSM(MOD_LGUI), OSM(MOD_LALT), OSM(MOD_LCTL), OSM(MOD_LSFT), KC_TRNS,               KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_0,
-        KC_TRNS,       KC_TRNS,       KC_TRNS,       KC_TRNS,       KC_TRNS,               KC_9,    KC_5,    KC_6,    KC_7,    KC_8,
+        KC_TRNS,       KC_TRNS,       KC_TAB,        KC_TRNS,       KC_TRNS,      KC_NO,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,
+        OSM(MOD_LGUI), OSM(MOD_LALT), OSM(MOD_LCTL), OSM(MOD_LSFT), KC_TRNS,               KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_BSPC,
+        KC_TRNS,       KC_TRNS,       KC_TRNS,       KC_TRNS,       KC_TRNS,               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
                                                      KC_TRNS,       KC_TRNS,               KC_TRNS, KC_TRNS
     ),
 	[_NAV] = LAYOUT(
         KC_LBRC,    KC_RBRC,    KC_LCBR, KC_RCBR, KC_NO, KC_NO, KC_NO,   KC_GRV,  KC_TILD, KC_PIPE,    KC_BSLS,
-        KC_EQL,     KC_MINS,    KC_SCLN, KC_COLN, KC_NO,        KC_NO,   KC_BSPC, KC_DEL,  RALT(KC_S), LSFT(KC_QUOT),
+        KC_EQL,     KC_MINS,    KC_SCLN, KC_COLN, KC_NO,        KC_NO,   SS_QUOT, SS_DQT,  RALT(KC_S), LSFT(KC_QUOT),
         KC_PLUS,    KC_UNDS,    KC_LPRN, KC_RPRN, KC_NO,        KC_NO,   KC_NO,   KC_NO,   KC_NO,      KC_NO,
                                          KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS
     ),
@@ -114,6 +115,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case SS_QUOT:
             if (record->event.pressed) {
                 SEND_STRING(SS_TAP(X_QUOT) SS_TAP(X_SPC));
+            }
+            return false;
+        case SS_DQT:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LSFT("'") SS_TAP(X_SPC));
             }
             return false;
         case SS_UE:
