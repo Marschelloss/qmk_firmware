9aa6e0fa3fe9797eed29c83b9c396a0416bdcd7a
Marcel Keßler <marcel.kessler@posteo.de>
2024-05-11 10:48:12 +0200

    Working layer tap + compose key

--- keyboards/totem/keymaps/donhansdampf/keymap.c
+++ keyboards/totem/keymaps/donhansdampf/keymap.c
@@ -75,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 KC_Q,            KC_W,     KC_E,        KC_R,           KC_T,               KC_Y,     KC_U,                 KC_I,     KC_O,     KC_P,
                 KC_A,            KC_S,     KC_D,        KC_F,           KC_G,               KC_H,     KC_J,                 KC_K,     KC_L,     KC_BSPC,
     KC_LCTL,    CTL_T(KC_Z),     KC_X,     KC_C,        KC_V,           KC_B,               KC_N,     KC_M,                 KC_COMM,  KC_DOT,   ALT_T(KC_SLSH),  KC_RALT,
-                                           KC_NO,       MO(_NUMBER),    LGUI_T(KC_SPC),     KC_LSFT,  LT(_NAVIGON, KC_TAB), KC_NO
+                                           KC_NO,       MO(_NUMBER),    LGUI_T(KC_SPC),     KC_LSFT,  LT(_NAVIGON, KC_DQUO), KC_NO
  ),
 
  /*
@@ -96,7 +96,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
    [_NUMBER] = LAYOUT(
  //╷         ╷         ╷         ╷         ╷         ╷         ╷╷         ╷         ╷         ╷         ╷         ╷         ╷
-                KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, KC_NUBS,   KC_PLUS,      KC_1,         KC_2,      KC_3,     KC_4,
+                KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, KC_BSLS,   KC_PLUS,      KC_1,         KC_2,      KC_3,     KC_4,
                 KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_TILD,   KC_LEFT,      KC_DOWN,      KC_UP,     KC_RIGHT, KC_MINS,
     DF(_MOUSE), KC_DQT,  KC_QUOT, KC_LPRN, KC_RPRN, KC_GRV,    KC_EQL,       KC_5,         KC_6,      KC_7,     KC_8,   KC_NO,
                                   KC_NO,   KC_NO,   KC_NO,     LSFT_T(KC_9), KC_0,         KC_NO
@@ -213,21 +213,27 @@ combo_t key_combos[COMBO_COUNT] = {
 // ▝▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▘
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case OS_SWAP:
-            if (record->event.pressed) {
-                if (!keymap_config.swap_lctl_lgui) {
-                  keymap_config.swap_lctl_lgui = false;  // ─── MAC
-                }
-                else {
-                  keymap_config.swap_lctl_lgui = true; // ─── WIN
-                }
-            eeconfig_update_keymap(keymap_config.raw);
-            clear_keyboard();  // ──── clear to prevent stuck keys    
-            return false;
-          }
+  switch (keycode) {
+  case OS_SWAP:
+    if (record->event.pressed) {
+      if (!keymap_config.swap_lctl_lgui) {
+        keymap_config.swap_lctl_lgui = false;  // ─── MAC
+      }
+      else {
+        keymap_config.swap_lctl_lgui = true; // ─── WIN
+      }
+      eeconfig_update_keymap(keymap_config.raw);
+      clear_keyboard();  // ──── clear to prevent stuck keys
+      return false;
     }
-    return true;
+  case LT(_NAVIGON, KC_DQUO):
+    if (record->tap.count && record->event.pressed) {
+      tap_code16(KC_DQUO);
+      return false;
+    }
+    break;
+  }
+return true;
 }
 
 void keyboard_pre_init_kb(void) {
