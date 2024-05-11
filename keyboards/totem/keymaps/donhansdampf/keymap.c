6339d376de92d9f9791d2a23a201651d51a70de4
Marcel Keßler <marcel.kessler@posteo.de>
2024-05-11 10:37:46 +0200

    Make totem useable ;)

--- keyboards/totem/keymaps/donhansdampf/keymap.c
+++ keyboards/totem/keymaps/donhansdampf/keymap.c
@@ -29,7 +29,6 @@ enum totem_layers {
     _BASED,
     _NUMBER,
     _NAVIGON,
-    _FUNCTION,
     _MOUSE
 };
 
@@ -40,7 +39,6 @@ enum totem_layers {
 enum custom_keycodes {
     NUM,
     NAV,
-    FUN,
     OS_SWAP,
     SNAP,
 };
@@ -48,24 +46,6 @@ enum custom_keycodes {
 // ┌─────────────────────────────────────────────────┐
 // │ d e f i n e   m a c r o n a m e s               │
 // └─────────────────────────────────────────────────┘
-
-// LEFT HAND HOME ROW MODS QWERTY ├──────────────────┐
-
-#define GUI_A MT(MOD_LGUI, KC_A)
-#define ALT_S MT(MOD_LALT, KC_S)
-#define CTL_D MT(MOD_LCTL, KC_D)
-#define SHT_F MT(MOD_LSFT, KC_F)
-
-// RIGHT HAND HOME ROW MODS QWERTY ├─────────────────┐
-
-#define SHT_J MT(MOD_RSFT, KC_J)
-#define CTL_K MT(MOD_RCTL, KC_K)
-#define ALT_L MT(MOD_RALT, KC_L)
-#define GUI_S MT(MOD_RGUI, KC_QUOT)
-
-#define NUM LT(_NUMBER, KC_TAB)
-#define NAV LT(_NAVIGON, KC_BSPC)
-#define FUN MO(_FUNCTION)
 
 #define XXX KC_NO
 
@@ -92,10 +72,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
    [_BASED] = LAYOUT(
  //╷         ╷         ╷         ╷         ╷         ╷         ╷╷         ╷         ╷         ╷         ╷         ╷         ╷
-                KC_Q,     KC_W,     KC_E,        KC_R,           KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,
-                KC_A,     KC_S,     KC_D,        KC_F,           KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_BSPC,
-    KC_LCTL,    KC_Z,     KC_X,     KC_C,        KC_V,           KC_B,      KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RALT,
-                                    MO(_NUMBER), LGUI_T(KC_SPC), KC_ESC,    KC_ENT, KC_LSFT,  MO(_NAVIGON)
+                KC_Q,            KC_W,     KC_E,        KC_R,           KC_T,               KC_Y,     KC_U,                 KC_I,     KC_O,     KC_P,
+                KC_A,            KC_S,     KC_D,        KC_F,           KC_G,               KC_H,     KC_J,                 KC_K,     KC_L,     KC_BSPC,
+    KC_LCTL,    CTL_T(KC_Z),     KC_X,     KC_C,        KC_V,           KC_B,               KC_N,     KC_M,                 KC_COMM,  KC_DOT,   ALT_T(KC_SLSH),  KC_RALT,
+                                           KC_NO,       MO(_NUMBER),    LGUI_T(KC_SPC),     KC_LSFT,  LT(_NAVIGON, KC_TAB), KC_NO
  ),
 
  /*
@@ -116,10 +96,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
    [_NUMBER] = LAYOUT(
  //╷         ╷         ╷         ╷         ╷         ╷         ╷╷         ╷         ╷         ╷         ╷         ╷         ╷
-                KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, KC_NUBS,   KC_PLUS,    KC_1,         KC_2,      KC_3,     KC_4,
-                KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_TILD,   KC_LEFT,    KC_DOWN,      KC_UP,     KC_RIGHT, KC_MINS,
-    DF(_MOUSE), KC_DQT,  KC_QUOT, KC_LPRN, KC_RPRN, KC_GRV,    KC_EQL,     KC_5,         KC_6,      KC_7,     KC_8,   KC_NO,
-                                  KC_NO,   KC_NO,   KC_NO,     KC_NO,      LSFT_T(KC_9), KC_0
+                KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, KC_NUBS,   KC_PLUS,      KC_1,         KC_2,      KC_3,     KC_4,
+                KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_TILD,   KC_LEFT,      KC_DOWN,      KC_UP,     KC_RIGHT, KC_MINS,
+    DF(_MOUSE), KC_DQT,  KC_QUOT, KC_LPRN, KC_RPRN, KC_GRV,    KC_EQL,       KC_5,         KC_6,      KC_7,     KC_8,   KC_NO,
+                                  KC_NO,   KC_NO,   KC_NO,     LSFT_T(KC_9), KC_0,         KC_NO
  ),
 
  /*
@@ -140,34 +120,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
    [_NAVIGON] = LAYOUT(
  //╷         ╷         ╷         ╷         ╷         ╷         ╷╷         ╷         ╷         ╷         ╷         ╷         ╷
-             _______,  _______,   _______,  _______, DM_REC1,   KC_MUTE,  KC_MPRV,  KC_MPLY,  KC_MNXT,  _______,
-             KC_LGUI,  KC_LALT,   KC_LCTL,  KC_LSFT, DM_PLY1,   KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT, CW_TOGG,
-    KC_LBRC, _______,  _______,   _______,  _______, DM_RSTP,   KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,   KC_PSCR, KC_RBRC,
-                                  _______,  _______, FUN,       _______,  KC_LSFT,  _______
+             KC_MUTE,   KC_MPRV,    KC_MPLY,   KC_MNXT,       DM_REC1,   KC_F11,   KC_F1,    KC_F2,    KC_F3,    KC_F4,
+             KC_LGUI,   KC_LALT,    KC_LCTL,   KC_LSFT,       DM_PLY1,   KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,   KC_DEL,
+    KC_NO,   _______,  _______,   _______,  _______,      DM_RSTP,   KC_F12,   KC_F5,    KC_F6,    KC_F7,    KC_F8,   KC_NO,
+                                    _______,  LGUI(KC_SPC),  KC_SPC,    _______, _______, _______
  ),
- /*
-   ╺━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╸
-   
-   ┌─────────────────────────────────────────────────┐
-   │ a d j u s t                                     │      ╭╮╭╮╭╮╭╮
-   └─────────────────────────────────────────────────┘      │╰╯╰╯╰╯│
-             ┌─────────┬─────────┬─────────┬─────────┬──────╨──┐┌──╨──────┬─────────┬─────────┬─────────┬─────────┐
-     ╌┄┈┈───═╡  RESET  │         │         │         │         ││         │   F7    │   F8    │   F9    │   F12   │   
-             ├─────────┼─────────┼─────────┼─────────┼─────────┤├─────────┼─────────┼─────────┼─────────┼─────────┤
-             │ DEBUG   │ QWERTY  │         │         │         ││         │   F4    │   F5    │   F6    │   F11   │   
-   ┌─────────┼─────────┼─────────┼─────────┼─────────┼─────────┤├─────────┼─────────┼─────────┼─────────┼─────────┼─────────┐
-   │  MAKE   │ OS SWAP │ COLEMAK │         │         │         ││         │   F1    │   F2    │   F3    │   F10   │   F13   │
-   └─────────┴─────────┴─────────┼─────────┼─────────┼─────────┤├─────────┼─────────┼─────────┼─────────┴─────────┴─────────┘
-                                 │    ▼    │    ▼    │    ▼    ││    ▼    │    ▼    │    ▼    │  
-                                 └─────────┴─────────┴─────────┘└─────────┴─────────┴─────────┘ */ 
 
-   [_FUNCTION] = LAYOUT(
- //╷         ╷         ╷         ╷         ╷         ╷         ╷╷         ╷         ╷         ╷         ╷         ╷         ╷
-              KC_F5,    KC_F6,    KC_F7,    KC_F8,    XXXXXXX,   XXXXXXX,  RGB_TOG,  RGB_M_T,  XXXXXXX,  QK_BOOT,
-              KC_F1,    KC_F2,    KC_F3,    KC_F4,    XXXXXXX,   XXXXXXX,  KC_RSFT,  KC_RCTL,  KC_RALT,  KC_RGUI,
-   KC_LCBR,   KC_F9,    KC_F10,   KC_F11,   KC_F12,   XXXXXXX,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_RCBR,
-                                  _______,  _______,  _______,   _______,  _______,  _______  
- ),
  /*
    ╺━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╸
 
@@ -186,10 +144,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
    [_MOUSE] = LAYOUT(
  //╷         ╷         ╷         ╷         ╷         ╷         ╷╷         ╷         ╷         ╷         ╷         ╷         ╷
-                _______,  KC_WH_D,  KC_MS_U,  KC_WH_U,   _______,  _______,  _______,  _______,  _______,   _______,
-                _______,  KC_MS_L,  KC_MS_D,  KC_MS_R,   _______,  _______,  _______,  _______,  _______,   _______,
-    DF(_BASED), _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,   _______, _______,
-                                    KC_BTN3,  KC_BTN2,   KC_BTN1,  _______,  _______,  _______
+                _______,  KC_WH_D,   KC_MS_U,   KC_WH_U,   _______,  _______,  _______,  _______,  _______,   _______,
+                _______,  KC_MS_L,   KC_MS_D,   KC_MS_R,   _______,  _______,  _______,  _______,  _______,   _______,
+    DF(_BASED), _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______, _______,
+                                      KC_BTN3,   KC_BTN2,   KC_BTN1,   _______,  _______,  _______
  )
 /*
    ╺━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╸
@@ -217,7 +175,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 */
 };
 
+// ┌────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┐
+// │ C O M B O S
+// └────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘
+// ▝▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▘
 
+enum combos {
+  D_F_ESC,
+  J_K_ENT,
+  M_COMM_COLN,
+  COMM_DOT_SCLN,
+  C_V_TAB,
+  X_C_QUOT,
+
+};
+
+const uint16_t PROGMEM d_f_esc[] = { KC_D, KC_F, COMBO_END};
+const uint16_t PROGMEM j_k_ent[] = { KC_J, KC_K, COMBO_END};
+const uint16_t PROGMEM m_comm_coln[] = { KC_M, KC_COMM, COMBO_END};
+const uint16_t PROGMEM comm_dot_scln[] = { KC_COMM, KC_DOT, COMBO_END};
+const uint16_t PROGMEM c_v_tab[] = { KC_C, KC_V, COMBO_END};
+const uint16_t PROGMEM x_c_quot[] = { KC_X, KC_C, COMBO_END};
+
+combo_t key_combos[COMBO_COUNT] = {
+  [D_F_ESC] = COMBO(d_f_esc, KC_ESC),
+  [J_K_ENT] = COMBO(j_k_ent, KC_ENT),
+  [M_COMM_COLN] = COMBO(m_comm_coln, KC_COLN),
+  [COMM_DOT_SCLN] = COMBO(comm_dot_scln, KC_SCLN),
+  [C_V_TAB] = COMBO(c_v_tab, KC_TAB),
+  [X_C_QUOT] = COMBO(x_c_quot, KC_QUOT),
+
+};
 
 // ┌────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┐
 // │ M A C R O S                                                                                                            │
@@ -238,9 +226,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             clear_keyboard();  // ──── clear to prevent stuck keys    
             return false;
           }
-// ┌─────────────────────────────────────────────────┐
-// │ p r o d u c t i v i t y                         │
-// └─────────────────────────────────────────────────┘
     }
     return true;
 }
@@ -266,8 +251,8 @@ void housekeeping_task_user(void) {
             rgblight_setrgb_at(RGB_CORAL, 1);
             break;
         case 2:
-            rgblight_setrgb_at(RGB_CORAL, 0);
-            rgblight_setrgb_at(RGB_CORAL, 1);
+            rgblight_setrgb_at(RGB_GREEN, 0);
+            rgblight_setrgb_at(RGB_GREEN, 1);
             break;
         case 3:
             rgblight_setrgb_at(RGB_GREEN, 0);
