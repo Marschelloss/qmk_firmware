ad114db6e41f41003dc946f1692b92159c8c1108
Marcel Keßler <marcel.kessler@posteo.de>
2024-05-27 10:11:59 +0200

    TAB on thumb?!

--- keyboards/tominabox1/le_chiffre/keymaps/donhansdampf/keymap.c
+++ keyboards/tominabox1/le_chiffre/keymaps/donhansdampf/keymap.c
@@ -34,10 +34,10 @@ enum custom_keycodes {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASED] = LAYOUT(
-                KC_Q,            KC_W,     KC_E,        KC_R,           KC_T,             KC_NO,  KC_Y,     KC_U,                 KC_I,     KC_O,     KC_P,
-                KC_A,            KC_S,     KC_D,        KC_F,           KC_G,                     KC_H,     KC_J,                 KC_K,     KC_L,     KC_BSPC,
-                CTL_T(KC_Z),     KC_X,     KC_C,        KC_V,           KC_B,                     KC_N,     KC_M,                 KC_COMM,  KC_DOT,   ALT_T(KC_SLSH),
-                                                        MO(_NUMBER),    LGUI_T(KC_SPC),           KC_LSFT,  LT(_SYSTEM, KC_DQUO)
+                KC_Q,            KC_W,     KC_E,        KC_R,                   KC_T,             KC_NO,  KC_Y,     KC_U,                 KC_I,     KC_O,     KC_P,
+                KC_A,            KC_S,     KC_D,        KC_F,                   KC_G,                     KC_H,     KC_J,                 KC_K,     KC_L,     KC_BSPC,
+                CTL_T(KC_Z),     KC_X,     KC_C,        KC_V,                   KC_B,                     KC_N,     KC_M,                 KC_COMM,  KC_DOT,   ALT_T(KC_SLSH),
+                                                        LT(_NUMBER, KC_TAB),    LGUI_T(KC_SPC),           KC_LSFT,  LT(_SYSTEM, KC_DQUO)
     ),
     [_NUMBER] = LAYOUT(
                 KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, KC_BSLS,    KC_NO,  KC_DOT,       KC_1,         KC_2,      KC_3,     KC_4,
