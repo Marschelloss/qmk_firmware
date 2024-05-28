b9f1118687383c8aa3ba40bbcf4a4d058a15aa2f
Marcel Keßler <marcel.kessler@posteo.de>
2024-05-28 15:45:51 +0200

    i'm going crazy

--- keyboards/tominabox1/le_chiffre/keymaps/donhansdampf/keymap.c
+++ keyboards/tominabox1/le_chiffre/keymaps/donhansdampf/keymap.c
@@ -29,12 +29,9 @@ enum custom_keycodes {
     SS_DQT,
     SS_TILD,
     SS_GRV,
-<<<<<<< HEAD
     RATE_4,
     RATE_5
-=======
     SS_DEAD,
->>>>>>> d824e47eed (save)
 };
 
 // clang-format off
@@ -43,11 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 KC_Q,            KC_W,     KC_E,        KC_R,                   KC_T,             KC_NO,  KC_Y,     KC_U,                 KC_I,     KC_O,     KC_P,
                 KC_A,            KC_S,     KC_D,        KC_F,                   KC_G,                     KC_H,     KC_J,                 KC_K,     KC_L,     KC_BSPC,
                 CTL_T(KC_Z),     KC_X,     KC_C,        KC_V,                   KC_B,                     KC_N,     KC_M,                 KC_COMM,  KC_DOT,   ALT_T(KC_SLSH),
-<<<<<<< HEAD
                                                         MO(_NUMBER),            LGUI_T(KC_SPC),           KC_LSFT,  LT(_SYSTEM, KC_DQUO)
-=======
-                                                        LT(_NUMBER, KC_TAB),    LGUI_T(KC_SPC),           KC_LSFT,  LT(_SYSTEM, KC_TAB)
->>>>>>> d824e47eed (save)
     ),
     [_NUMBER] = LAYOUT(
                 KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, KC_BSLS,    KC_NO,  KC_DOT,       KC_1,         KC_2,      KC_3,     KC_4,
@@ -62,17 +55,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                LGUI(KC_SPC),  KC_TAB,           QK_BOOT,  KC_NO
     ),
     [_MOUSE] = LAYOUT(
-<<<<<<< HEAD
                 DF(_BASED), KC_WH_D,   KC_MS_U,   KC_WH_U,   KC_NO,       KC_NO, KC_NO,   KC_PGUP,   KC_UP,     KC_PGDN,    KC_NO,
                 KC_NO,      KC_MS_L,   KC_MS_D,   KC_MS_R,   KC_NO,              RATE_5,  KC_LEFT,   KC_DOWN,   KC_RIGHT,   RATE_4,
                 KC_NO,      KC_NO,     KC_NO,     KC_NO,     KC_NO,              KC_NO,   KC_NO,     KC_NO,     KC_NO,      KC_NO,
                                                   KC_BTN2,   KC_BTN1,            KC_BTN2, KC_BTN1
-=======
-                KC_NO,  KC_WH_D,   KC_MS_U,   KC_WH_U,   KC_NO,       KC_NO, KC_NO,     KC_PGUP,   KC_UP,     KC_PGDN,    KC_NO,
-                KC_NO,  KC_MS_L,   KC_MS_D,   KC_MS_R,   DF(_BASED),         KC_NO,     KC_LEFT,   KC_DOWN,   KC_RIGHT,   KC_NO,
-                KC_NO,  KC_NO,     KC_NO,     KC_NO,     KC_NO,              KC_NO,     KC_NO,     KC_NO,     KC_NO,      KC_NO,
-                                              KC_BTN2,   KC_BTN1,            KC_BTN2,   KC_BTN1
->>>>>>> d824e47eed (save)
     )
 };
 // clang-format on
