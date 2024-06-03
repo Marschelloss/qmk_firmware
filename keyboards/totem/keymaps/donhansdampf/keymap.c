872b7c9b6b66e3ff33828f5d78584e6af4bf6643
Marcel Kessler <marcel.kessler@datev.de>
2024-06-03 09:04:05 +0200

    Remove tab

--- keyboards/totem/keymaps/donhansdampf/keymap.c
+++ keyboards/totem/keymaps/donhansdampf/keymap.c
@@ -82,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 KC_Q,            KC_W,     KC_E,        KC_R,                   KC_T,               KC_Y,     KC_U,                  KC_I,     KC_O,     KC_P,
                 KC_A,            KC_S,     KC_D,        KC_F,                   KC_G,               KC_H,     KC_J,                  KC_K,     KC_L,     KC_BSPC,
     DF(_MOUSE), CTL_T(KC_Z),     KC_X,     KC_C,        KC_V,                   KC_B,               KC_N,     KC_M,                  KC_COMM,  KC_DOT,   ALT_T(KC_SLSH),  KC_RALT,
-                                           KC_NO,       LT(_NUMBER, KC_TAB),    LGUI_T(KC_SPC),     KC_LSFT,  LT(_NAVIGON, KC_DQUO), KC_NO
+                                           KC_NO,       MO(_NUMBER),            LGUI_T(KC_SPC),     KC_LSFT,  LT(_NAVIGON, KC_DQUO), KC_NO
  ),
  /*
    ╺━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╸
