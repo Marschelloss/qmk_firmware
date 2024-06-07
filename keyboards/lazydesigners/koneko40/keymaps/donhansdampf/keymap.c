f8dc45e1ca74e52b83de7005c13f8938da864018
Marcel Keßler <marcel.kessler@posteo.de>
2024-06-08 00:50:50 +0200

    Add Game Mode

--- keyboards/lazydesigners/koneko40/keymaps/donhansdampf/keymap.c
+++ keyboards/lazydesigners/koneko40/keymaps/donhansdampf/keymap.c
@@ -5,6 +5,8 @@ enum layers {
     _NUMBER,
     _SYMBOL,
     _MOUSE,
+    _GAME,
+    _GAMENR,
     _EMPTY
 };
 
@@ -29,26 +31,47 @@ enum custom_keycodes {
 #define L_SYM LT(_SYMBOL, KC_DQUO)
 #define L_MSE DF(_MOUSE)
 #define L_BSE DF(_BASED)
+#define L_GMN MO(_GAMENR)
 
+#define GAME TO(_GAME)
+#define BASE TO(_BASED)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASED] = LAYOUT_ortho(
-        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_NO,    KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     GAME,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,
         KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_NO,    KC_H,     KC_J,     KC_K,     KC_L,     KC_BSPC,
         KC_LALT,  C_CTL,    KC_X,     KC_C,     KC_V,     KC_B,     KC_NO,    KC_N,     KC_M,     KC_COMM,  KC_DOT,   C_ALT,
         KC_ESC,   KC_NO,    KC_NO,    KC_NO,    L_NUM,    C_CMD,    KC_NO,    KC_LSFT,  L_SYM,    KC_NO,    KC_NO,    KC_NO
     ),
     [_NUMBER] = LAYOUT_ortho(
         KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_1,     KC_2,     KC_3,     KC_4,
-        KC_NO,    KC_LGUI,  KC_LALT,  KC_LCTL,  KC_LSFT,  KC_NO,    KC_NO,    KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,   KC_MINS,
+        KC_NO,    KC_LGUI,  KC_LALT,  KC_LCTL,  KC_LSFT,  KC_NO,    KC_NO,    KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT, KC_MINS,
         KC_NO,    KC_PLUS,  KC_EQL,   KC_DOT,   KC_COMM,  KC_NO,    KC_NO,    KC_NO,    KC_5,     KC_6,     KC_7,     KC_8,
         KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    C_SFT,    KC_0,     KC_NO,    KC_NO,    KC_NO
     ),
     [_SYMBOL] = LAYOUT_ortho(
-        KC_NO,    KC_LBRC,  KC_RBRC,  KC_LCBR,  KC_RCBR,  KC_NO,    KC_NO,    KC_F11,   KC_F1,    KC_F2,    KC_F3,    KC_F4,
-        KC_NO,    KC_LGUI,  KC_LALT,  KC_LCTL,  KC_LSFT,  KC_NO,    KC_NO,    KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT, KC_DEL,
-        KC_NO,    KC_BSLS,  KC_PIPE,  KC_LPRN,  KC_RPRN,  KC_NO,    KC_NO,    KC_F12,   KC_F5,    KC_F6,    KC_F7,    KC_F8,
-        KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_TAB,   C_SPC,    KC_NO,    QK_BOOT,  KC_NO,    KC_NO,    KC_NO,    KC_NO
+        KC_NO,    KC_LBRC,  KC_RBRC,  KC_LCBR,  KC_RCBR,  KC_NO,    KC_NO,    KC_F12,   KC_F1,    KC_F2,    KC_F3,    KC_F4,
+        KC_NO,    KC_LGUI,  KC_LALT,  KC_LCTL,  KC_LSFT,  KC_NO,    KC_NO,    KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,   KC_DEL,
+        KC_NO,    KC_BSLS,  KC_PIPE,  KC_LPRN,  KC_RPRN,  KC_NO,    KC_NO,    KC_F13,   KC_F5,    KC_F6,    KC_F7,    KC_F8,
+        QK_BOOT,  KC_NO,    KC_NO,    KC_NO,    KC_TAB,   C_SPC,    KC_NO,    KC_NO,    KC_NO,    KC_F9,    KC_F10,   KC_F11
+    ),
+    [_MOUSE] = LAYOUT_ortho(
+        KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+        KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+        KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+        KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO
+    ),
+    [_GAME] = LAYOUT_ortho(
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_NO,    KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,
+        KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_PGUP,  KC_H,     KC_J,     KC_K,     KC_L,     KC_BSPC,
+        KC_LALT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_PGDN,  KC_N,     KC_M,     KC_COMM,  KC_DOT,   C_ALT,
+        KC_ESC,   KC_B,     KC_G,     KC_LSFT,  L_GMN,    KC_SPC,   BASE,     KC_LSFT,  L_SYM,    KC_NO,    KC_NO,    KC_ENT
+    ),
+    [_GAMENR] = LAYOUT_ortho(
+        KC_5,     KC_1,     KC_2,     KC_3,     KC_4,     KC_NO,    KC_NO,    KC_NO,    KC_1,     KC_2,     KC_3,     KC_4,
+        KC_6,     KC_TRNS,  KC_2,     KC_TRNS,  KC_7,     KC_NO,    KC_NO,    KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT, KC_MINS,
+        KC_TRNS,  KC_8,     KC_9,     KC_0,     KC_TRNS,  KC_NO,    KC_NO,    KC_NO,    KC_5,     KC_6,     KC_7,     KC_8,
+        KC_TRNS,  KC_NO,    KC_NO,    KC_TRNS,  KC_NO,    KC_TRNS,  KC_NO,    C_SFT,    KC_0,     KC_NO,    KC_NO,    KC_NO
     ),
     [_EMPTY] = LAYOUT_ortho(
         KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
