3ca6f60859d1ee976c47e89702cb454066fdc0a1
Marcel Kessler <marcel.kessler@datev.de>
2024-09-11 14:58:08 +0200

    Fix dot,comma placemant

--- keyboards/totem/keymaps/donhansdampf/keymap.c
+++ keyboards/totem/keymaps/donhansdampf/keymap.c
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_NUMBER] = LAYOUT(
                   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,     KC_NO,    KC_1,     KC_2,     KC_3,     KC_4,
                   KC_LGUI,  KC_LALT,  KC_LCTL,  KC_LSFT,  KC_NO,     KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT, KC_MINS,
-        KC_NO,    KC_PLUS,  KC_EQL,   KC_DOT,   KC_COMM,  KC_NO,     KC_NO,    KC_5,     KC_6,     KC_7,     KC_8,     KC_NO,
+        KC_NO,    KC_PLUS,  KC_EQL,   KC_COMM,  KC_DOT,   KC_NO,     KC_NO,    KC_5,     KC_6,     KC_7,     KC_8,     KC_NO,
                                       KC_NO,    KC_NO,    KC_NO,     C_SFT,    KC_0,     KC_NO
     ),
     [_SYMBOL] = LAYOUT(
