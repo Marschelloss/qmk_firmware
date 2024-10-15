305b4fdb3bd5ba7784775d27a2318465da08e80f
Marcel Kessler <marcel.kessler@datev.de>
2024-10-15 11:36:48 +0200

    Added Gamelayer to TOTEM :scream:

--- keyboards/totem/keymaps/donhansdampf/keymap.c
+++ keyboards/totem/keymaps/donhansdampf/keymap.c
@@ -22,6 +22,8 @@ enum totem_layers {
     _NUMBER,
     _SYMBOL,
     _MOUSE,
+    _GAME,
+    _GAMENR,
 };
 
 enum custom_keycodes {
@@ -44,9 +46,12 @@ enum custom_keycodes {
 #define L_NUM MO(_NUMBER)
 #define L_SYM LT(_SYMBOL, KC_DQUO)
 #define L_MSE LT(_MOUSE, KC_LCTL)
+#define L_GMN MO(_GAMENR)
 
 // Layer Switch
 #define BASE TO(_BASED)
+#define GAME TO(_GAME)
+#define MOSE TO(_MOUSE)
 
 // Shortcuts
 #define C_SPC LGUI(KC_SPC)
@@ -55,13 +60,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASED] = LAYOUT(
                   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,
                   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_BSPC,
-        L_MSE,    C_CTL,    KC_X,     KC_C,     KC_V,     KC_B,      KC_N,     KC_M,     KC_COMM,  KC_DOT,   C_ALT,    KC_RALT,
+        KC_TAB,   C_CTL,    KC_X,     KC_C,     KC_V,     KC_B,      KC_N,     KC_M,     KC_COMM,  KC_DOT,   C_ALT,    KC_RALT,
                                       KC_ESC,   L_NUM,    C_CMD,     KC_LSFT,  L_SYM,    KC_ENT
     ),
     [_NUMBER] = LAYOUT(
                   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,     KC_NO,    KC_1,     KC_2,     KC_3,     KC_4,
-                  KC_LGUI,  KC_LALT,  KC_LCTL,  KC_LSFT,  KC_NO,     KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT, KC_MINS,
-        KC_NO,    KC_PLUS,  KC_EQL,   KC_COMM,  KC_DOT,   KC_NO,     KC_NO,    KC_5,     KC_6,     KC_7,     KC_8,     KC_NO,
+                  KC_LGUI,  KC_LALT,  KC_LCTL,  KC_LSFT,  MOSE,      KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT, KC_MINS,
+        KC_NO,    KC_PLUS,  KC_EQL,   KC_COMM,  KC_DOT,   GAME,      KC_NO,    KC_5,     KC_6,     KC_7,     KC_8,     KC_NO,
                                       KC_NO,    KC_NO,    KC_NO,     C_SFT,    KC_0,     KC_NO
     ),
     [_SYMBOL] = LAYOUT(
@@ -72,9 +77,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [_MOUSE] = LAYOUT(
                   KC_NO,    KC_WH_D,  KC_MS_U,  KC_WH_U,  KC_NO,     KC_NO,    KC_PGDN,  KC_UP,    KC_PGUP,  KC_NO,
-                  KC_BTN4,  KC_MS_L,  KC_MS_D,  KC_MS_R,  KC_BTN5,   KC_NO,    KC_LEFT,  KC_DOWN,  KC_RIGHT, KC_NO,
-        BASE,     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+                  KC_NO,    KC_MS_L,  KC_MS_D,  KC_MS_R,  BASE,      KC_NO,    KC_LEFT,  KC_DOWN,  KC_RIGHT, KC_NO,
+        KC_NO,    KC_NO,    KC_BTN4,  KC_NO,    KC_BTN5,  KC_NO,     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
                                       KC_BTN3,  KC_BTN2,  KC_BTN1,   KC_NO,    KC_NO,    KC_NO
+    ),
+    [_GAME] = LAYOUT(
+                  KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,      KC_NO,    KC_PGDN,  KC_UP,    KC_PGUP,  KC_NO,
+                  KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,      KC_NO,    KC_LEFT,  KC_DOWN,  KC_RIGHT, KC_NO,
+        KC_ESC,   KC_LALT,  KC_Z,     KC_X,     KC_C,     KC_V,      KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+                                      KC_LSFT,  L_GMN,    KC_SPC,    BASE,     KC_NO,    KC_NO
+    ),
+    [_GAMENR] = LAYOUT(
+                  KC_TRNS,  KC_1,     KC_2,     KC_3,     KC_TRNS,      KC_TRNS,    KC_TRNS,  KC_TRNS,    KC_TRNS,   KC_TRNS,
+                  KC_TRNS,  KC_4,     KC_5,     KC_6,     KC_TRNS,      KC_TRNS,    KC_TRNS,  KC_TRNS,    KC_TRNS,   KC_TRNS,
+        KC_ENT,   KC_TRNS,  KC_7,     KC_8,     KC_9,     KC_TRNS,      KC_TRNS,    KC_TRNS,  KC_TRNS,    KC_TRNS,   KC_TRNS,    KC_TRNS,
+                                      KC_TRNS,  KC_TRNS,  KC_TRNS,      KC_TRNS,    KC_TRNS,  KC_TRNS
     )
 };
 
